2d399bf6d2510bc3c4eebb5c6a817aae2b3a24e0
tomer <tomertal52@yahoo.com>
2022-11-20 20:02:24 -0500

    commit tree 52cbad70fc9827cc9c25ecb19577ec18d9cfc2b8

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+  if (distance <= 400) {
+     result = 'This one is on me!';
+  }
+  else if (distance >= 400 && distance <= 2000) {
+    result = 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip) {
+    case "generous" :
+      result = 'Thank you so much.';
+      break;
+    case "not as generous" : 
+      result = 'Thank you.';
+      break;
+    case "thanks for everything" :
+      result = 'Bye.';
+      break;
+  }
+  return result;
 }
